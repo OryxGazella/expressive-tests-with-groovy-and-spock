527583d1c70ab9056db4f0f88607dadf18273d0a
Frank Smith <oryx.gazella@gmail.com>
2016-03-01 00:51:25 +0200

    Move anonymous class to named inner static class

--- src/main/java/soy/frank/flutterby/Main.java
+++ src/main/java/soy/frank/flutterby/Main.java
@@ -11,39 +11,42 @@ public class Main {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.height = 480;
         config.width = 800;
-        new LwjglApplication(new ApplicationListener() {
-            @Override
-            public void create() {
+        new LwjglApplication(new Listener(), config);
+    }
 
-            }
+    public static class Listener implements ApplicationListener {
 
-            @Override
-            public void resize(int width, int height) {
+        @Override
+        public void create() {
 
-            }
+        }
 
-            @Override
-            public void render() {
-                // Sets the clear screen color to: Cornflower Blue
-                Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
-                // Clears the screen
-                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            }
+        @Override
+        public void resize(int width, int height) {
 
-            @Override
-            public void pause() {
+        }
 
-            }
+        @Override
+        public void render() {
+            // Sets the clear screen color to: Cornflower Blue
+            Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+            // Clears the screen
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        }
 
-            @Override
-            public void resume() {
+        @Override
+        public void pause() {
 
-            }
+        }
 
-            @Override
-            public void dispose() {
+        @Override
+        public void resume() {
 
-            }
-        }, config);
+        }
+
+        @Override
+        public void dispose() {
+
+        }
     }
 }
