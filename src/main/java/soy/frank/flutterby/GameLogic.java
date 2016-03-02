51702b132c587f754b8b130fc21c938d62fd2c51
Frank Smith <oryx.gazella@gmail.com>
2016-03-02 02:00:40 +0200

    Add identity function to the game logic class

---
+++ src/main/java/soy/frank/flutterby/GameLogic.java
@@ -0,0 +1,10 @@
+package soy.frank.flutterby;
+
+import soy.frank.flutterby.types.ButterflyControls;
+
+public class GameLogic {
+    //Start off with the identity function
+    public static float[] applyLogic(float[] xy, ButterflyControls butterflyControls) {
+        return xy;
+    }
+}
