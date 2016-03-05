c94a200a4bcc1d956d68bd6a04c9f9a5d2aa8f5a
Frank Smith <francois.smith@dstvdm.com>
2016-03-06 01:35:19 +0200

    Make tests more readable
    * Use where: blocks and @unroll in order to parameterize tests
    * Create DSLy type methods for creating test objects

--- src/test/java/soy/frank/flutterby/GameLogicTest.groovy
+++ src/test/java/soy/frank/flutterby/GameLogicTest.groovy
@@ -6,79 +6,50 @@ import soy.frank.flutterby.actors.Laser
 import soy.frank.flutterby.actors.PhysicalEntity
 import soy.frank.flutterby.input.ButterflyControls
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class GameLogicTest extends Specification {
 
-    public static final float velocity = 0.005f
-    public static final initialX = 0.0f
-    public static final initialY = 0.0f
-    public static
-    final initialScene = ImmutableScene.builder().butterfly(PhysicalEntity.createButterflyAt(initialX, initialY)).build() as ImmutableScene
+    public static final float velocity = Butterfly.VELOCITY
+    def static initialScene = ImmutableScene.builder().butterfly(PhysicalEntity.createButterflyAt(0f, 0f)).build() as ImmutableScene
 
-    def "Butterfly moves to the right"() {
-        given:
-        def movingRight = Stub(ButterflyControls) {
-            moveRight() >> true
+    @Unroll("A butterfly #moving is displaced by #deltaX on the x-axis and #deltaY on the y-axis")
+    def "A butterfly moves according to the controls"() {
+        expect:
+        with(GameLogic.applyLogic(initialScene, move(directions)).butterfly().position()) {
+            x() == deltaX
+            y() == deltaY
         }
 
-        when:
-        def result = GameLogic.applyLogic(initialScene, movingRight)
-
-        then:
-        result.butterfly().position().x() == (initialX + velocity).toFloat()
+        where:
+        moving                              | deltaX    | deltaY    | directions
+        "standing still"                    | 0f        | 0f        | []
+        "moving up"                         | 0f        | velocity  | ["up"]
+        "moving diagonally right upwards"   | velocity  | velocity  | ["up", "right"]
+        "moving right"                      | velocity  | 0f        | ["right"]
+        "moving diagonally right downwards" | velocity  | -velocity | ["right", "down"]
+        "moving down"                       | 0f        | -velocity | ["down"]
+        "moving diagonally left downwards"  | -velocity | -velocity | ["left", "down"]
+        "moving left"                       | -velocity | 0f        | ["left"]
+        "moving diagonally left upwards"    | -velocity | velocity  | ["left", "up"]
     }
 
-    def "Butterfly moves to the left"() {
-        given:
-        def movingLeft = Stub(ButterflyControls) {
-            moveLeft() >> true
+    def move(List<String> directions) {
+        def controlsStub = Stub(ButterflyControls)
+        directions.each {
+            controlsStub."move${it.capitalize()}"() >> true
         }
-
-        when:
-        def result = GameLogic.applyLogic(initialScene, movingLeft)
-
-        then:
-        result.butterfly().position().x() == (initialX - velocity).toFloat()
-    }
-
-    def "Butterfly moves up"() {
-        given:
-        def movingUp = Stub(ButterflyControls) {
-            moveUp() >> true
-        }
-
-        when:
-        def xy = GameLogic.applyLogic(initialScene, movingUp)
-
-        then:
-        xy.butterfly().position().y() == (initialY + velocity).toFloat()
-    }
-
-    def "Butterfly moves down"() {
-        given:
-        def movingDown = Stub(ButterflyControls) {
-            moveDown() >> true
-        }
-
-        when:
-        def xy = GameLogic.applyLogic(initialScene, movingDown)
-
-        then:
-        xy.butterfly().position().y() == (initialY - velocity).toFloat()
+        controlsStub
     }
 
     def "Butterfly spawns a laser in the center of its head when the fire control is sent"() {
-        given:
-        def firing = Stub(ButterflyControls) {
-            fire() >> true
-        }
-
-        when:
-        def resultingScene = GameLogic.applyLogic(initialScene, firing)
-
-        then:
-        resultingScene ==
+        expect:
+        GameLogic.applyLogic(initialScene, fire()) ==
                 initialScene
                         .withLasers(PhysicalEntity.createLaserAt(Butterfly.WIDTH / 2 - Laser.WIDTH / 2 as float, Butterfly.HEIGHT))
+    }
+
+    ButterflyControls fire() {
+        Stub(ButterflyControls) { fire() >> true }
     }
 }
