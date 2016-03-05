bafa44c0d4a37bbb53c18ad2b4d5f96203288fa5
Frank Smith <oryx.gazella@gmail.com>
2016-03-05 19:23:33 +0200

    Optimize rendering for 1920 x 1080
    Include sources of images in Readme as well as edited Vector gfx.

--- src/main/java/soy/frank/flutterby/actors/Butterfly.java
+++ src/main/java/soy/frank/flutterby/actors/Butterfly.java
@@ -1,8 +1,8 @@
 package soy.frank.flutterby.actors;
 
 public class Butterfly {
-    public static final float WIDTH = 0.08f;
-    public static final float HEIGHT = 0.08f;
+    public static final float WIDTH = 0.05f;
+    public static final float HEIGHT = 0.05f;
     public static final float VELOCITY = 0.005f;
 
     private Butterfly() {
